ab687f08dd5e4ed63a32c80da11ccc6a5f5615be
Celso Junior <celsoestercio.jr@gmail.com>
2021-04-28 23:19:38 -0300

    feat: create navBar and navBarItem component

--- src/main/App.js
+++ src/main/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import 'bootswatch/dist/flatly/bootstrap.css'
 import Routes from './routes'
 import '../custom.css'
+import NavBar from '../components/nav-bar';
+
 class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <Routes/>
-      </div>
+      <>
+        <NavBar />
+        <div className="container">
+          <Routes />
+        </div>
+      </>
     )
   }
 }
