3a4a8cb6c167207e7908b0b4ebfedea04303f18c
Celso Junior <celsoestercio.jr@gmail.com>
2021-04-28 22:50:40 -0300

    feat: create user signup

---
+++ src/views/user-signup.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import Card from '../components/card'
+import FormGroup from '../components/form-group'
+
+class UserSignup extends React.Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+
+    save = () => {
+        console.log(this.state)
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <Card title="Cadastro de usuário">
+                    <div className="row">
+                        <div className="col-lg-12">
+                            <div className="bs-component">
+                                <FormGroup label="Nome: *" htmlFor="inputName">
+                                    <input className="form-control" type="text" id="inputName" name="name" onChange={ e => this.setState({ name: e.target.value })}/>
+                                </FormGroup>
+                                <FormGroup label="Email: *" htmlFor="inputEmail">
+                                    <input className="form-control" type="email" id="inputEmail" name="email" onChange={ e => this.setState({ email: e.target.value })}/>
+                                </FormGroup>
+                                <FormGroup label="Senha: *" htmlFor="inputPassword">
+                                    <input className="form-control" type="password" id="inputPassword" name="password" onChange={ e => this.setState({ password: e.target.value })}/>
+                                </FormGroup>
+                                <FormGroup label="Confirmar senha: *" htmlFor="inputConfirmPassword">
+                                    <input className="form-control" type="password" id="inputConfirmPassword" name="confirmPassword" onChange={ e => this.setState({ confirmPassword: e.target.value })}/>
+                                </FormGroup>
+                                <button onClick={ this.save } className="btn btn-success">Salvar</button>
+                                <button onClick={ this.save } className="btn btn-danger">Cancelar</button>
+                            </div>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        )
+    }
+}
+
+export default UserSignup
